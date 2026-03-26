35ca3771090d761aefbd991a391f6da8167130c2
Yash Goswami <yashgoswami16102004@gmail.com>
2026-03-26 17:17:29 +0530

    Add debug and console log for server startspace changes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const debug = debugLib("gseb-ht-gen-backend:index");
 const PORT = config.get('App.config.port') || 3000;
 
 app.listen(PORT, () => {
+    
     debug(`server started on port ${PORT} (${process.env.NODE_ENV})`);
     console.log(`🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
 });
