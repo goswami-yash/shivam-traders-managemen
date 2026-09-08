858fa39506277f99b68499bfbcb213d2dcf19649
yash7803 <goswamiyash7803@gmail.com>
2026-09-08 14:31:07 +0530

    dns add

--- server/server.js
+++ server/server.js
@@ -255,7 +255,9 @@ const staticOrigins = [
   "http://192.168.0.66:8080",
   "http://192.168.0.66:80",
   "http://192.168.0.65:8080",
-  "http://65.0.107.128:8080"
+  "http://65.0.107.128:8080",
+  "http://sivamtraders-load-balancer-1490317179.ap-south-1.elb.amazonaws.com/"
+
 ];
 
 const buildAllowedOrigins = () => {
